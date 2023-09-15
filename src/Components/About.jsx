82dd039e30f0717ae4f223ebf8c123f3f91fb06a
Hamza Sajid <104695796+Hamzaqtr-313@users.noreply.github.com>
2023-09-16 01:10:48 +0500

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/technology-1283624_1280.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "coding background picture";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Software Engineering student studying at University of Engineering and Technology,Taxila. I enjoy creating unique and different softwares.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
+  "Web developer",
+  "Dev-Ops",
+  "Cloud-Computing",
+  "Azure",
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "My Portfolio Website using different technologies.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/willow-warbler-8071472_1280.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "bird picture";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Portfolio",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my portfolio website.",
+    url: "https://hamzaqtr-313.github.io/CodSoft/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "My Blog",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is my blog website.",
+    url: "https://hamzaqtr-313.github.io/Hamza/",
   },
   {
-    title: "My Resume Site",
+    title: "My Quran App",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Quran and Nasheed App.",
+    url: "https://quranic-nasheed-website.web.app/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "My Restaurant Website",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is a Restaurant Website.",
+    url: "https://restaurant-ef3ee.web.app/",
   },
 ];
 
